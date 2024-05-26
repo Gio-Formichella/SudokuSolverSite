bcb9846eaf6e63033a7fcf77ff5005545da9c717
Gio Formichella <gioformichella@gmail.com>
2024-05-26 16:53:26 +0200

    Rendering basic home and details pages

--- mysite/sudoku_solver/urls.py
+++ mysite/sudoku_solver/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("solver/", views.solve_sudoku_puzzle, name="solver"),
+    path("details/", views.detail, name="details")
 ]
--- mysite/sudoku_solver/views.py
+++ mysite/sudoku_solver/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world !")
+def solve_sudoku_puzzle(request, puzzle: list):
+    solved = False
+    while not solved:
+        assignments = []  # TODO: check board variable assignments
+        context = {
+            "assignments": assignments
+        }
+        return render(request, "sudoku_solver/home", context)
+
+
+def detail(request):
+    context = {
+        "name": "Gio"
+    }
+    return render(request, "sudoku_solver/details.html", context)
+
+
+def home(request):
+    return render(request, "sudoku_solver/home.html")
