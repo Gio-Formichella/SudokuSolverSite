9c8cadb497f2b2507fd489ad945dd73cf971000e
Gio Formichella <gioformichella@gmail.com>
2024-06-07 18:54:24 +0200

    Removed redis

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,14 +42,6 @@ INSTALLED_APPS = [
     "channels",
 ]
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)]
-        },
-    },
-}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
