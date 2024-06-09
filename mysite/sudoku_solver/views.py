206227f4c2eab3c8d1c60503ecbea997f5c3a9bc
Gio Formichella <gioformichella@gmail.com>
2024-06-09 19:55:31 +0200

    Removed non utilized methods

--- mysite/sudoku_solver/views.py
+++ mysite/sudoku_solver/views.py
@@ -2,16 +2,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def solve_sudoku_puzzle(request, puzzle: list):
-    solved = False
-    while not solved:
-        assignments = []  # TODO: check board variable assignments
-        context = {
-            "assignments": assignments
-        }
-        return render(request, "sudoku_solver/home", context)
-
-
 def detail(request):
     context = {
         "name": "Gio"
@@ -21,7 +11,3 @@ def detail(request):
 
 def home(request):
     return render(request, "sudoku_solver/home.html")
-
-
-def sudoku_board(request):
-    return render(request, "sudoku_solver/sudoku_board.html", {})
