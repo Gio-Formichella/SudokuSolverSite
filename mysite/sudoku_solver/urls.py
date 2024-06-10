5ce4509330c0f8f34f5ce7dc1311984728cc730f
Gio Formichella <gioformichella@gmail.com>
2024-06-10 17:33:55 +0200

    Removed un unutilized url pattern

--- mysite/sudoku_solver/urls.py
+++ mysite/sudoku_solver/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("solver/", views.solve_sudoku_puzzle, name="solver"),
     path("details/", views.detail, name="details")
 ]
