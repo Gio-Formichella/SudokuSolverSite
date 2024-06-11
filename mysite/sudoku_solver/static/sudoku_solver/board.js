9e026ade1e69e8cd1f629095920ea1b1e51371df
Gio Formichella <gioformichella@gmail.com>
2024-06-11 18:10:40 +0200

    Added board flash and error message when puzzle has no solution

--- mysite/sudoku_solver/static/sudoku_solver/board.js
+++ mysite/sudoku_solver/static/sudoku_solver/board.js
@@ -3,7 +3,9 @@ console.log(ws)
 
 const solveButton = document.querySelector('.solve-button');
 const sudokuBoard = document.querySelector('.board');
+const messageElement = document.querySelector('.solution-message');
 
+let puzzle; // Will hold user puzzle
 
 function getPuzzle() {
   const puzzle = [];
@@ -19,10 +21,6 @@ function getPuzzle() {
   return puzzle;
 }
 
-solveButton.addEventListener('click', () => {
-  const puzzle = getPuzzle();
-  ws.send(JSON.stringify(puzzle));
-});
 
 function updateBoard(solutionData) {
   for (let row = 0; row < 9; row++) {
@@ -33,8 +31,43 @@ function updateBoard(solutionData) {
   }
 }
 
+function flashBoardRed() {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = document.getElementById(`cell-${row}-${col}`);
+      cell.style.background = '#f00'; // Set red background
+      setTimeout(() => {
+          cell.style.background = '#fff'; // Reset to white background
+      }, 200); // Delay (in milliseconds)
+    }
+  }
+}
+
+function displayMessage(message, type) {
+    if (type == "no-solution") {
+        messageElement.textContent = message;
+        messageElement.classList.add('error-message');
+    }
+}
+
+solveButton.addEventListener('click', () => {
+  puzzle = getPuzzle();
+  ws.send(JSON.stringify(puzzle));
+  messageElement.classList.remove('error-message');
+});
+
 ws.onmessage = function(event) {
     const message = JSON.parse(event.data);
-    board = message[0]
-    updateBoard(board);
+    if (message === null) {
+        // puzzle has no solution
+        flashBoardRed();
+        displayMessage("Puzzle has no solution", "no-solution")
+        if (puzzle){
+            updateBoard(puzzle)
+        }
+    }
+    else {
+        board = message[0]
+        updateBoard(board);
+    }
 };
