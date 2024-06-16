e3f4529aeed62274eac769ec7a2b0e54e2c49753
Gio Formichella <gioformichella@gmail.com>
2024-06-16 18:11:04 +0200

    Organized received messages based on type, made send_assignment_update method

--- mysite/sudoku_solver/consumers.py
+++ mysite/sudoku_solver/consumers.py
@@ -27,20 +27,35 @@ class BoardConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         # Method called when server receives data from the client over websocket
-        received = json.loads(text_data)  # Is a puzzle or a reset message
+        received = json.loads(text_data)  # Holds a puzzle or a reset message
         match received["type"]:
             case "reset":
                 self.solver_running = False
             case "solve":
                 self.solver_running = True
-                board, assignments, backtracks, = backtracking_solver(received["puzzle"])
+                board, assignments, backtracks = await backtracking_solver(puzzle=received["puzzle"])
 
                 if self.solver_running:  # No reset message has been received
                     message = json.dumps({
+                        "type": "solve",
                         "board": board,
                         "msg": f"Solution found with {assignments} assignments and {backtracks} backtracks"
                     }, cls=NumpyArrayEncoder)
                     await self.send(text_data=message)
 
             case "step-by-step":
-                pass
+                self.solver_running = True
+                # Method will call send_assignment_update
+                await backtracking_solver(puzzle=received["puzzle"], consumer=self)
+
+    async def send_assignment_update(self, row: int, col: int, value: int, assignments: int, backtracks: int):
+        if self.solver_running:  # Non reset message has been received
+            message = json.dumps({
+                "type": "step-by-step",
+                "row": row,
+                "col": col,
+                "value": value,
+                "msg": f"Current state found with {assignments} assignments and {backtracks} backtracks",
+                "count": assignments  # Using assignment count for message ordering
+            })
+            await self.send(text_data=message)
